534dd59b2971360e2a537ea83236ce7a01ac8af4
Kai Beltz <kainbeltz@gmail.com>
2026-07-21 22:07:25 -0400

    wip: add temporary sign-in/sign-up links to home screen

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -2,6 +2,7 @@ import { Text } from '@/components/ui/text';
 import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useHealth } from '@/hooks/useHealthCheck';
+import { Link } from 'expo-router';
 
 function HealthProfile() {
   const { data, isPending, isError, error } = useHealth();
@@ -15,11 +16,23 @@ function HealthProfile() {
   }
 
   return (
+  <>
     <View>
       <Text>{data?.status}</Text>
       <Text>Backend Connected</Text>
     </View>
-  );
+
+    <Link href="/sign-in">
+      <Text>Go to Sign In</Text>
+    </Link>
+
+
+    <Link href="/sign-up">
+      <Text>Go to Sign Up</Text>
+    </Link>
+  </>
+);
+
 }
 
 export default function Home() {
